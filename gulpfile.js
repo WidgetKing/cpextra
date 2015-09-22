2424197e0b6019e76abd9e241b0b8462c86fc607
Tristan Ward <tristan.ward@infosemantics.com.au>
2015-09-22 12:02:47 +0800

    Added Intellij project files and gulp workflow automation

---
+++ gulpfile.js
@@ -0,0 +1,15 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: Tristan
+ * Date: 22/09/15
+ * Time: 11:54 AM
+ * To change this template use File | Settings | File Templates.
+ */
+var gulp = require("gulp"),
+        gutil = require("gulp-util");
+
+gulp.task("log",function() {
+
+    gutil.log("Our workflow is up and running!");
+
+});
