215ee648e968031d4562e0cce520ae0d59ba0313
WidgetKing <$tSdw$LqS0&#>
2017-04-17 13:18:59 +0800

    Extracted _extra.variableManager.processCommandVariableRegistration into separate file.

--- components/scripts/js/dev/managers/variables/command/commandVariables.global.js
+++ components/scripts/js/dev/managers/variables/command/commandVariables.global.js
@@ -5,7 +5,7 @@
  * Time: 8:14 AM
  * To change this template use File | Settings | File Templates.
  */
-_extra.registerModule("commandVariables_global", ["commandVariableManager", "slideObjectUtilMethods", "localStorageManager", "parameterParseSets"], function () {
+_extra.registerModule("commandVariables_global", ["processCommandVariableRegistration", "localStorageManager", "parameterParseSets"], function () {
 
     "use strict";
 
@@ -142,75 +142,7 @@ _extra.registerModule("commandVariables_global", ["commandVariableManager", "sli
             }
         };
 
-    _extra.variableManager.processCommandVariableRegistration = function (data) {
-        /*var example = {
-            "VariableSuffix":{
-                "parameterHandler":handlers.sendParametersAsParameters
-                "commandName":"enableMouseEvents",
-                "updateData":function (data, a, b) {
 
-                }
-                "parseSet":_extra.variableManager.parseSets.SP.CD.SOR,
-                "parseSetData":{
-                    "anything":null
-                }
-            }
-        };*/
-
-        function entryPoint() {
-            for (var variableName in data) {
-                if (data.hasOwnProperty(variableName)) {
-
-                    registerVariable(variableName, data[variableName]);
-
-                }
-            }
-        }
-
-        function registerVariable(variableName, data) {
-
-            function entryPoint() {
-
-                if (data.commandName) {
-
-                    _extra.variableManager.commands[data.commandName] = handleVariableChange;
-
-                }
-
-                // Register
-                _extra.variableManager.registerCommandVariable(variableName, handleVariableChange,
-                    data.parameterHandler);
-
-            }
-
-            function handleVariableChange () {
-
-                if (data.updateData) {
-
-                    // Make the first parameter the parseSetData
-                    _extra.w.Array.prototype.unshift.call(arguments, data.parseSetData);
-                    // Update data to the parameters we've received
-                    data.updateData.apply(this, arguments);
-
-                    data.parseSet(data.parseSetData);
-
-                } else {
-
-                    _extra.error("You forgot to add a data.updateData method");
-
-                }
-
-            }
-
-            entryPoint();
-
-        }
-
-
-        entryPoint();
-
-
-    };
 
 
     _extra.variableManager.processCommandVariableRegistration({
